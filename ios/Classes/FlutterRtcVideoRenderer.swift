d04912673981016f6ed4695362646ac39d047b51
alexlapa <lapa.alex@ex.ua>
2023-11-14 17:50:06 +0200

    fix iOs video renderer

--- ios/Classes/FlutterRtcVideoRenderer.swift
+++ ios/Classes/FlutterRtcVideoRenderer.swift
@@ -251,14 +251,18 @@ class FlutterRtcVideoRenderer: NSObject, FlutterTexture, RTCVideoRenderer {
     }
 
     let buffer = self.correctRotation(frame: renderFrame!)
-    let isFrameWidthChanged = self.frameWidth != renderFrame!.buffer.width
-    let isFrameHeightChanged = self.frameHeight != renderFrame!.buffer.height
+    let isFrameWidthChanged = self.frameWidth != buffer.width
+    let isFrameHeightChanged = self.frameHeight != buffer.height
+    let isFrameRotationChanged = self.frameRotation != rotation
+
     if isFrameWidthChanged
       || isFrameHeightChanged
-      || self.frameRotation != rotation
+      || isFrameRotationChanged
     {
-      self.frameWidth = renderFrame!.buffer.width
-      self.frameHeight = renderFrame!.buffer.height
+      self.frameWidth = buffer.width
+      self.frameHeight = buffer.height
+      self.frameRotation = rotation
+
       self.broadcastEventObserver().onTextureChange(
         id: self.textureId,
         height: self.frameHeight,
