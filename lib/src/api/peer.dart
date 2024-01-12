9e94a1a00f92b9e9060107476482cd933a14d9e0
rogurotus <rogurotus@gmail.com>
2024-01-12 13:54:49 +0300

    fix

--- lib/src/api/peer.dart
+++ lib/src/api/peer.dart
@@ -339,8 +339,8 @@ class _PeerConnectionChannel extends PeerConnection {
 
     print("OPA42");
     if (mediaType == MediaKind.video) {
-      var h = SendEncodingParameters.create("h", true, scalabilityMode: "L3T3");
-      init.sendEncodings.add(h);
+      // var h = SendEncodingParameters.create("h", true, scalabilityMode: "L3T3");
+      // init.sendEncodings.add(h);
       // init.sendEncodings.add(m);
       // init.sendEncodings.add(l);
     } else {
