2fb56da0f42aa9dbcc52588276b7e6e1a0642789
rogurotus <rogurotus@gmail.com>
2024-05-07 02:09:23 +0600

    retry

--- ios/Classes/proxy/PeerConnectionProxy.swift
+++ ios/Classes/proxy/PeerConnectionProxy.swift
@@ -1,35 +1,5 @@
 import WebRTC
 
-class Logger {
-
-    static var logFile: URL? {
-        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
-        let fileName = "LOGLOG.log"
-        return documentsDirectory.appendingPathComponent(fileName)
-    }
-
-    static func log(_ message: String) {
-        guard let logFile = logFile else {
-            return
-        }
-
-        let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm:ss"
-        let timestamp = formatter.string(from: Date())
-        guard let data = (timestamp + ": " + message + "\n").data(using: String.Encoding.utf8) else { return }
-
-        if FileManager.default.fileExists(atPath: logFile.path) {
-            if let fileHandle = try? FileHandle(forWritingTo: logFile) {
-                fileHandle.seekToEndOfFile()
-                fileHandle.write(data)
-                fileHandle.closeFile()
-            }
-        } else {
-            try? data.write(to: logFile, options: .atomicWrite)
-        }
-    }
-}
-
 /// Wrapper around a `PeerConnection`, powering it with additional API..
 class PeerConnectionProxy {
   /// List of `IceCandidate`s added before a remote description has been set on
